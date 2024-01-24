f8ff28ea40e26c71adda01bad6f5861f04884cf1
zsaritek <zsaritek@gmail.com>
2024-01-24 17:54:11 +0100

    regenaration of accesstoken

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -4,6 +4,22 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Feedback = require("../models/Feedback.model");
 const Company = require("../models/Company.model");
 const User = require("../models/User.model");
+const crypto = require("crypto");
+
+router.patch("/company", isAuthenticated, async (req, res) => {
+    try {
+        const { _id } = req.payload;
+        const user = await User.findOne({ _id }).populate('company');
+        if (!user || !user.company) {
+            return res.status(404).json({ message: "Not Found" })
+        }
+        await Company.updateOne({ _id: user.company._id, accessToken: crypto.randomBytes(20).toString('hex') });
+        const company = await Company.findOne({ _id: user.company._id })
+        return res.status(200).json(company)
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+})
 
 router.get("/company", isAuthenticated, async (req, res) => {
     try {
